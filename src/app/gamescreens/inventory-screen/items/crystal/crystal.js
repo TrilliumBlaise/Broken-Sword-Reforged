f690328290a2947446a10a45813c01a6ab4ea865
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-01 13:49:38 -0400

    Update crystal.js

--- src/app/gamescreens/inventory-screen/items/crystal/crystal.js
+++ src/app/gamescreens/inventory-screen/items/crystal/crystal.js
@@ -1,34 +1,16 @@
-export default class Crystal {
-    //Propeties
-    crystalElement;
-    rarity;
-    id;
+export class Crystal {
+  constructor(element, id) {
+    this.crystalElement = element;
+    this.rarity = setRarity();
+    this.id = id;
+  }
+}
 
-    //Constructor
-    constructor(element, id) {
-        this.crystalElement = element;
-        this.rarity = Crystal.setRarity();
-        this.id = id;
-    }
-
-    get element() {
-        return this.element;
-    }
-
-    //Returns a random rarity level for a crystal
-    static setRarity() {
-        const ran = Math.floor(Math.random() * 10);
-        switch (ran) {
-            case 0: return 'Basic';
-            case 1: return 'Basic';
-            case 2: return 'Basic';
-            case 3: return 'Basic';
-            case 4: return 'Rare';
-            case 5: return 'Rare'
-            case 6: return 'Rare'
-            case 7: return 'Epic'
-            case 8: return 'Epic'
-            case 9: return 'Legendary'
-        }
-    }
+//Returns a random rarity level for a crystal
+function setRarity() {
+  const ran = Math.floor(Math.random() * 10);
+  if (ran < 4) return 'Basic';
+  if (ran < 7) return 'Rare';
+  if (ran < 9) return 'Epic';
+  return 'Legendary';
 }
