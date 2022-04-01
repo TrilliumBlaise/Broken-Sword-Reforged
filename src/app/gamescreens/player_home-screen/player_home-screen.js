76385ed0b688b6fca48861723ba582e2e12c95d1
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-01 13:56:25 -0400

    Update player_home-screen.js

--- src/app/gamescreens/player_home-screen/player_home-screen.js
+++ src/app/gamescreens/player_home-screen/player_home-screen.js
@@ -1,5 +1,5 @@
-import PlayerAPI from '../../player/PlayerAPI.js'
-import CurrentPlayerAPI from '../../player/CurrentPlayerAPI.js'
+import PlayerAPI from '../../player/PlayerAPI.js';
+import CurrentPlayerAPI from '../../player/CurrentPlayerAPI.js';
 
 const player = CurrentPlayerAPI.read();
 const currentAction = document.querySelector('#current');
@@ -7,67 +7,68 @@ const textOutput = document.querySelector('.text');
 
 //Allows the player to make the inventory screen visible and invisible
 document.querySelector('.inventory-button').addEventListener('click', () => {
-    const inventory = document.querySelector('.inventory');
-    if (inventory.classList.contains('fadeOut')) {
-        inventory.classList.replace('fadeOut', 'fadeIn');
-        inventory.style.position = 'relative';
-        return;
-    }
-    if (inventory.classList.contains('fadeIn')) {
-        inventory.classList.replace('fadeIn', 'fadeOut');
-        inventory.style.position = 'absolute';
-        return;
-    }
+  const inventory = document.querySelector('.inventory');
+  if (inventory.classList.contains('fadeOut')) {
+    inventory.classList.replace('fadeOut', 'fadeIn');
+    inventory.style.position = 'relative';
+    return;
+  }
+  if (inventory.classList.contains('fadeIn')) {
+    inventory.classList.replace('fadeIn', 'fadeOut');
+    inventory.style.position = 'absolute';
+    return;
+  }
 });
 
 //Allows the player to save and quit the game
 document.querySelector('.yes').addEventListener('click', () => {
-    console.log(currentAction.innerHTML);
-    switch (currentAction.innerHTML) {
-        case 'save': {
-            const savePlayer = CurrentPlayerAPI.read();
-            const players = PlayerAPI.getPlayers();
-            let foundPlayerFlag = false;
-            for (const player of players) {
-                if (player.name != savePlayer.name) {
-                    continue;
-                }
-                foundPlayerFlag = true;
-            }
-            if (!foundPlayerFlag) {
-                PlayerAPI.saveNewPlayer(savePlayer)
-            }
-            if (foundPlayerFlag) {
-                PlayerAPI.updatePlayer(savePlayer);
-            }
-            textOutput.innerHTML = 'Your game has been saved.<br> Would you like to quit?'
-            currentAction.innerHTML = 'quit'
-            break;
+  switch (currentAction.innerHTML) {
+    case 'save': {
+      const savePlayer = CurrentPlayerAPI.read();
+      const players = PlayerAPI.getPlayers();
+      let foundPlayerFlag = false;
+      for (const player of players) {
+        if (player.name != savePlayer.name) {
+          continue;
         }
-        case 'quit': {
-            window.location.reload();
-            break;
-        }
+        foundPlayerFlag = true;
+      }
+      if (!foundPlayerFlag) {
+        PlayerAPI.saveNewPlayer(savePlayer);
+      }
+      if (foundPlayerFlag) {
+        PlayerAPI.updatePlayer(savePlayer);
+      }
+      textOutput.innerHTML =
+        'Your game has been saved.<br> Would you like to quit?';
+      currentAction.innerHTML = 'quit';
+      break;
     }
+    case 'quit': {
+      window.reload();
+      break;
+    }
+  }
 });
 
 //Allows the player to not save and not quit the game
 document.querySelector('.no').addEventListener('click', () => {
-    switch (currentAction.innerHTML) {
-        case 'save': {
-            textOutput.innerHTML = 'Your game has NOT been saved.<br> Would you like to quit?'
-            currentAction.innerHTML = 'quit'
-            break;
-        }
-        case 'quit': {
-            currentAction.innerHTML = 'save';
-            document.querySelector('.inventory').innerHTML = ''
-            window.location.href = '../village-screen/village-screen.html'
-            break;
-        }
+  switch (currentAction.innerHTML) {
+    case 'save': {
+      textOutput.innerHTML =
+        'Your game has NOT been saved.<br> Would you like to quit?';
+      currentAction.innerHTML = 'quit';
+      break;
     }
+    case 'quit': {
+      currentAction.innerHTML = 'save';
+      document.querySelector('.inventory').innerHTML = '';
+      window.location.href = '../village-screen/village-screen.html';
+      break;
+    }
+  }
 });
 
-document.querySelector('.inventory').innerHTML = `<iframe src= '../inventory-screen/inventory-screen.html' scrolling= 'no'></iframe>`
-
-
+document.querySelector(
+  '.inventory'
+).innerHTML = `<iframe src= '../inventory-screen/inventory-screen.html' scrolling= 'no'></iframe>`;
