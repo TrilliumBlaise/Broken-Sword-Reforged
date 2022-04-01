1a4aa503b73ce610b766b84631fbc57048df24f5
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-01 13:51:33 -0400

    Update weapon_type.js

--- src/app/gamescreens/inventory-screen/items/weapon/weapon_type.js
+++ src/app/gamescreens/inventory-screen/items/weapon/weapon_type.js
@@ -1,42 +1,38 @@
-export default class WeaponType {
-    static DAGGER = new WeaponType('Dagger', 4, 2.0, 75);
-    static SWORD = new WeaponType('Sword', 10, 1.0, 100);
-    static CLUB = new WeaponType('Club', 15, 0.9, 150);
-    static SPEAR = new WeaponType('Spear', 10, 1.5, 75);
-    static AXE = new WeaponType('Axe', 20, 0.8, 150);
-    static SHIELD = new WeaponType('Shield', 15, 1.1, 200);
-
-    type = '';
-    power = 0;
-    speed = 0;
-    durability = 0;
-
-    static weaponTypeArray = [WeaponType.DAGGER, WeaponType.SWORD, WeaponType.CLUB, WeaponType.SPEAR, WeaponType.AXE, WeaponType.SHIELD];
-
-    constructor(type, power, speed, durability) {
-        this.type = type;
-        this.power = power;
-        this.speed = speed;
-        this.durability = durability;
-    }
-
-    get type() {
-        return this.type;
-    }
-
-    get power() {
-        return this.power;
-    }
-
-    get speed() {
-        return this.speed;
-    }
-
-    get durability() {
-        return this.durability;
-    }
-
-    get weaponTypeArray() {
-        return WeaponType.weaponTypeArray
-    }
-}
+export const weapons = [
+  {
+    type: 'Dagger',
+    power: 4,
+    speed: 2.0,
+    durability: 75,
+  },
+  {
+    type: 'Sword',
+    power: 10,
+    speed: 1.0,
+    durability: 100,
+  },
+  {
+    type: 'Club',
+    power: 15,
+    speed: 0.9,
+    durability: 150,
+  },
+  {
+    type: 'Spear',
+    power: 10,
+    speed: 1.5,
+    durability: 75,
+  },
+  {
+    type: 'Axe',
+    power: 20,
+    speed: 0.8,
+    durability: 150,
+  },
+  {
+    type: 'Shield',
+    power: 15,
+    speed: 1.1,
+    durability: 200,
+  },
+];
