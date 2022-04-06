f89809e8bbd5419e04dbb350d2f7f3eb73c2f0c9
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-06 12:49:18 -0400

    Update enemies.js

--- src/app/enemy/enemies.js
+++ src/app/enemy/enemies.js
@@ -1,7 +1,9 @@
 //Enemies array is order from weakest to strongest
+/*Slime count = 0-2, Animal count = 0-3, Undead count = 0-5, Beast count = 3-7, Dragon count = 5-9, Corrupted count = 7->10*/
 export const enemies = [
   {
     name: ['Slime'],
+    count: { min: 0, max: 2 },
     //Stat arrays are orderd by [HP, Power, Speed]
     statsBelowLevel5: [50, 3, 1.0],
     statsBelowLevel10: [100, 6, 1.5],
@@ -9,6 +11,7 @@ export const enemies = [
   },
   {
     name: ['Bear', 'Tiger', 'Lion', 'Boar'],
+    count: { min: 0, max: 3 },
     //Stat arrays are orderd by [HP, Power, Speed]
     statsBelowLevel5: [75, 5, 1.3],
     statsBelowLevel10: [100, 14, 1.0],
@@ -16,6 +19,7 @@ export const enemies = [
   },
   {
     name: ['Wraith', 'Zombie', 'Lich', 'Vampire'],
+    count: { min: 0, max: 5 },
     //Stat arrays are orderd by [HP, Power, Speed]
     statsBelowLevel5: [75, 15, 0.7],
     statsBelowLevel10: [150, 30, 1.2],
@@ -23,6 +27,7 @@ export const enemies = [
   },
   {
     name: ['Yeti', 'Werewolf', 'Chimera', 'Chlorofiend'],
+    count: { min: 3, max: 7 },
     //Stat arrays are orderd by [HP, Power, Speed]
     statsBelowLevel5: [75, 15, 1.3],
     statsBelowLevel10: [150, 30, 1.0],
@@ -30,6 +35,7 @@ export const enemies = [
   },
   {
     name: ['Dragon'],
+    count: { min: 5, max: 9 },
     //Stat arrays are orderd by [HP, Power, Speed]
     statsBelowLevel5: [150, 20, 1.1],
     statsBelowLevel10: [300, 40, 1.6],
@@ -37,6 +43,7 @@ export const enemies = [
   },
   {
     name: ['Corrupted'],
+    count: { min: 7, max: 1000 },
     //Stat arrays are orderd by [HP, Power, Speed]
     statsBelowLevel5: [150, 15, 1.0],
     statsBelowLevel10: [300, 30, 1.5],
@@ -50,3 +57,4 @@ export const enemies = [
     statsElse: [900, 65, 2.2],
   },
 ];
+
