21016fec92b176e30a38b616baa671773b34ab7a
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-07 08:27:09 -0400

    Update enemy.js

--- src/app/enemy/enemy.js
+++ src/app/enemy/enemy.js
@@ -4,7 +4,8 @@ import Player from '../player/player.js';
 
 let creature;
 export function createEnemy(player, count) {
-  if (!player) {
+  if (!player)
+    return {
       name: enemies[enemies.length - 1].name,
       enemyElement: elements[4],
       drop: undefined,
@@ -13,23 +14,42 @@ export function createEnemy(player, count) {
       speed: enemies[enemies.length - 1].statsBelowLevel10[2],
       takeDamage,
     };
-  const enemy = {
-    name: setName(count),
-    enemyElement: elements[getRandomNumber(4)],
-    drop: setDrop(player, count),
-    hp: 0,
-    power: 0,
-    speed: 0,
-    takeDamage,
-  };
+  const sprite = setSprite(count);
+  const enemy = [
+    '',
+    {
+      name: sprite[1],
+      sprite: sprite[0],
+      enemyElement: elements[getRandomNumber(4)],
+      drop: setDrop(player, count),
+      hp: 0,
+      power: 0,
+      speed: 0,
+      takeDamage,
+    },
+  ];
   const stats = setStats(player);
-  enemy.hp = stats[0];
-  enemy.maxHP = enemy.hp;
-  enemy.power = stats[1];
-  enemy.speed = stats[2];
-  console.log(enemy.name);
+  enemy[1].hp = stats[0];
+  enemy[1].maxHP = enemy[1].hp;
+  enemy[1].power = stats[1];
+  enemy[1].speed = stats[2];
+  enemy[0] = createElement(enemy[1]);
   return enemy;
 }
+
+function createElement(enemy) {
+  const enemyDiv = document.createElement('div');
+  enemyDiv.classList.add('enemy');
+  enemyDiv.style.backgroundImage = enemy.sprite.backgroundImage;
+  enemyDiv.style.backgroundRepeat = enemy.sprite.backgroundRepeat;
+  enemyDiv.style.backgroundPosition = enemy.sprite.backgroundPosition;
+  enemyDiv.style.backgroundSize = `${enemy.sprite.backgroundSize[0]}% ${enemy.sprite.backgroundSize[1]}%`;
+  const enemyInfo = document.createElement('div');
+  enemyInfo.classList.add('enemy-hp');
+  enemyDiv.append(enemyInfo);
+  return enemyDiv;
+}
+
 function takeDamage(player) {
   let isCrit = false;
   if (!player.inventory) {
@@ -59,27 +79,26 @@ function getRandomNumber(number) {
 //returns the creature type of this
 //@Param count
 function setCreature(count) {
-  const i = getRandomNumber(3);
   const countEnemies = enemies.filter(enemy => {
     if (enemy.count?.min <= count && count <= enemy.count?.max) return enemy;
   });
   if (countEnemies.length < 3) countEnemies.push(countEnemies[0]);
-  console.log(countEnemies);
-  return countEnemies[i];
+  return countEnemies[getRandomNumber(countEnemies.length)];
 }
 //returns the name of this
-//@Param count
-function setName(count) {
+//@Param creature, element
+function setSprite(count) {
   creature = setCreature(count);
-  return creature.name[getRandomNumber(creature.name.length)];
+  const index = getRandomNumber(creature.name.length);
+  return [creature.sprites[index], creature.name[index]];
 }
 
 //@params player, count
 //returns the drop of this
 function setDrop(player, count) {
-  const i = getRandomNumber(6);
-  if (i < 3) return 'weapon';
-  if (i < 5) return 100 + 5 * player.level * count;
+  const i = getRandomNumber(10);
+  if (i < 8) return 'weapon';
+  if (i < 9) return 100 + 5 * player.level * count;
   return 'orb';
 }
 //returns the stats of this [hp, power, speed]
