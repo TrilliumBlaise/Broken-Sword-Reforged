c6e76244393a381652c60afa9926c915e24f70b2
TrilliumBlaise <79470625+TrilliumBlaise@users.noreply.github.com>
2022-04-08 10:29:31 -0400

    Update story-screen.js

--- src/app/gamescreens/story-screen/story-screen.js
+++ src/app/gamescreens/story-screen/story-screen.js
@@ -1,28 +1,46 @@
-const paragraphs = document.querySelectorAll('.p');
-const currentParagraph = document.querySelector('.current-paragraph');
+const paragraphs = document.querySelectorAll('[data-current]');
 const body = document.querySelector('body');
-
-body.addEventListener('click', () => {
-    switch (currentParagraph.innerHTML) {
-        case 'paragraph-1': {
-            paragraphs[0].classList.replace('fadeIn', 'fadeOut');
-            paragraphs[1].classList.replace('fadeOut', 'fadeIn');
-            currentParagraph.innerHTML = 'paragraph-2';
-            break;
-        }
-        case 'paragraph-2': {
-            paragraphs[1].classList.replace('fadeIn', 'fadeOut');
-            paragraphs[2].classList.replace('fadeOut', 'fadeIn');
-            currentParagraph.innerHTML = 'paragraph-3';
-            break;
-        }
-        case 'paragraph-3': {
-            paragraphs[2].classList.replace('fadeIn', 'fadeOut');
-            paragraphs[3].classList.replace('fadeOut', 'fadeIn');
-            currentParagraph.innerHTML = 'paragraph-4';
-            break;}
-        case 'paragraph-4': {
-            window.location.href = '../home-screen/home-screen.html';
-            break;}
+body.addEventListener('keydown', e => {
+  if (e.key === 'ArrowLeft') {
+    changeCurrentParagraph(false, true);
+  }
+  if (e.key === 'ArrowRight') {
+    changeCurrentParagraph(true, false);
+  }
+});
+document.querySelectorAll('div').forEach(div => {
+  div.addEventListener('click', e => {
+    if (e.shiftKey) {
+      e.preventDefault();
+      changeCurrentParagraph(false, true);
+      return;
     }
-})
+    changeCurrentParagraph(true, false);
+  });
+});
+
+function changeCurrentParagraph(next = false, previous = false) {
+  let current;
+  for (let i = 0; i < paragraphs.length; i++) {
+    if (paragraphs[i].dataset.current === 'true') current = paragraphs[i];
+  }
+  if (next) {
+    if (!current.nextElementSibling.classList.contains('p')) {
+      window.location.href = '../home-screen/home-screen.html';
+      return;
+    }
+    current.classList.replace('fadeIn', 'fadeOut');
+    current.dataset.current = 'false';
+    current.nextElementSibling.classList.replace('fadeOut', 'fadeIn');
+    current.nextElementSibling.dataset.current = 'true';
+  }
+  if (previous) {
+    if (!current.previousElementSibling.classList.contains('p')) {
+      return;
+    }
+    current.classList.replace('fadeIn', 'fadeOut');
+    current.dataset.current = 'false';
+    current.previousElementSibling.classList.replace('fadeOut', 'fadeIn');
+    current.previousElementSibling.dataset.current = 'true';
+  }
+}
